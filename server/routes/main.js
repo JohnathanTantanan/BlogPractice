a924ce821c697a310e942f96841c6735fa46142b
JohnathanTantanan <164973394+JohnathanTantanan@users.noreply.github.com>
2025-03-04 17:37:00 +0800

    Added the main pages
    -createpost
    -communities
    -home
    -login
    -myprofile
    -popular
    
    they link to each other but for some reason the css blew up
    
    things to fix:
    -login
    -myprofile
    
    imma take a break

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,17 +1,37 @@
 const express = require('express')
 const router = express.Router()
 
+const locals = {
+    title: "NodeJs Blog",
+    description: "Simple Blog created with NodeJs, Express & MongoDB."
+}
 
 router.get('',(req,res)=>{
-    const locals = {
-        title: "NodeJs Blog",
-        description: "Simple Blog created with NodeJs, Express & MongoDB."
-    }
-    res.render('index', { locals, layout: 'layouts/main' })
+    res.render('home', { locals, layout: 'layouts/main' })
 })
 
-router.get('/about',(req,res)=>{
-    res.render('about')
+router.get('/home',(req,res)=>{
+    res.render('home', { locals, layout: 'layouts/main' })
+})
+
+router.get('/login',(req,res)=>{
+    res.render('login', { locals, layout: 'layouts/main' })
+})
+
+router.get('/communities',(req,res)=>{
+    res.render('communities', { locals, layout: 'layouts/main' })
+})
+
+router.get('/createpost',(req,res)=>{
+    res.render('createpost', { locals, layout: 'layouts/main' })
+})
+
+router.get('/myprofile',(req,res)=>{
+    res.render('myprofile', { locals, layout: 'layouts/main' })
+})
+
+router.get('/popular',(req,res)=>{
+    res.render('popular', { locals, layout: 'layouts/main' })
 })
 
 module.exports = router
